3290c1a7b5d13af1c6a658a8df39ed2e9a834203
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-04-01 17:27:49 +0200

    feat: AuthLoading screen to lead proper route

--- src/navigation/AuthenticateStack/AuthenticateStack.tsx
+++ src/navigation/AuthenticateStack/AuthenticateStack.tsx
@@ -1,13 +1,13 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React from 'react';
-import HomeScreen from '../../container/HomeScreen';
+import AuthLoading from '../../screens/AuthenticatingPage/AuthLoading';
 
 const AuthAtack = createNativeStackNavigator();
 
 function AuthenticateStack() {
   return (
-    <AuthAtack.Navigator>
-      <AuthAtack.Screen name="AuthStack" component={HomeScreen} />
+    <AuthAtack.Navigator screenOptions={{ headerShown: false }}>
+      <AuthAtack.Screen name="AuthStack" component={AuthLoading} />
     </AuthAtack.Navigator>
   );
 }
