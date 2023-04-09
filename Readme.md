e55fb4d8234840491177127cdee7683760e22dcc
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-04-09 17:08:49 +0200

    feat: reactotron config/ disable log into app

---
+++ Readme.md
@@ -0,0 +1,4 @@
+
+## Run Reactotron
+To watch Api responses, async storage manipulation, can run reactoron into app, before that open Reactotron application and then run below commend to sync android device;
+> adb reverse tcp:9090 tcp:9090
--- index.js
+++ index.js
@@ -2,8 +2,14 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+
+if (__DEV__) {
+  import('./src/config/reactotronConfig').then(() =>
+    console.log('Reactotron Configured'),
+  );
+}
 
 AppRegistry.registerComponent(appName, () => App);
