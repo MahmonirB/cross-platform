384a9da19d708847a5b6a3e29c511c6369a73a90
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-04-09 17:08:49 +0200

    feat: reactotron config/ disable log into app

--- App.tsx
+++ App.tsx
@@ -2,8 +2,10 @@ import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
 import RootNavigator from './src/navigation/AppNavigator';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { LogBox } from 'react-native';
 
 const clientQuery = new QueryClient();
+LogBox.ignoreAllLogs(true);
 
 const App = () => (
   <QueryClientProvider client={clientQuery}>
