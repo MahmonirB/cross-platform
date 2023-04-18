935331c243f3648d2501b7216adb06f8216b4b88
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-04-19 00:45:56 +0200

    feat: add react-native language detector

--- src/config/i18n.js
+++ src/config/i18n.js
@@ -1,9 +1,11 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import i18nextReactNative from 'i18next-react-native-language-detector';
 import { initReactI18next } from 'react-i18next';
+import { WEB_ENV } from '.';
 
 i18n
-  .use(LanguageDetector)
+  .use(WEB_ENV ? LanguageDetector : i18nextReactNative)
   .use(initReactI18next)
   .init({
     resources: {
