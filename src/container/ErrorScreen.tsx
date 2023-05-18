80ea35e3e4d2281de9ac15206a59114a679d8767
Mahmonir Bakhtiyari <37932243+MahmonirB@users.noreply.github.com>
2023-05-18 23:56:01 +0200

    feat: apply retry functionality

--- src/container/ErrorScreen.tsx
+++ src/container/ErrorScreen.tsx
@@ -1,14 +1,24 @@
 import { colors } from '@app/styles/colors';
 import * as React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import LazyImage from '@app/components/LazyImage';
+import { replace } from '@app/navigation/RootNavigation';
+import { useNetInfo } from '@react-native-community/netinfo';
 
 function ErrorScreen() {
+  const { isConnected, isInternetReachable } = useNetInfo();
+  const handleRefresh = () => {
+    if (isConnected && isInternetReachable) {
+      replace('Main');
+    }
+  };
+
   return (
     <View style={styles.container}>
       <LazyImage name="networkError" />
       <Text style={styles.title}>Oops! 404</Text>
       <Text style={styles.caption}>Network Error</Text>
+      <Button onPress={handleRefresh} title="Retry" />
     </View>
   );
 }
@@ -29,6 +39,13 @@ const styles = StyleSheet.create({
     fontSize: 14,
     fontWeight: '500',
     color: colors.bluePrimary,
+    marginBottom: 24,
+  },
+  btn: {
+    backgroundColor: colors.primary,
+    color: colors.white,
+    width: 200,
+    height: 40,
   },
 });
 
